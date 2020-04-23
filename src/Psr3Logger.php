c666c06086ed08f9e7641c7c121bffd6381fbf47
Eduardo Weiland <eduardo@eduardoweiland.info>
2020-04-23 03:58:31 -0300

    Escape parameters with quote method from connection driver (#2)

--- src/Psr3Logger.php
+++ src/Psr3Logger.php
@@ -16,30 +16,41 @@
 namespace Tuupola\DBAL\Logging;
 
 use Doctrine\DBAL\Logging\SQLLogger;
+use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\SQLParserUtils;
 
 class Psr3Logger implements SQLLogger
 {
     public $logger;
+    public $connection;
     public $sql = "";
     public $start = null;
 
-    public function __construct($logger = null)
+    public function __construct($logger, Connection $connection)
     {
         $this->logger = $logger;
+        $this->connection = $connection;
     }
 
     public function startQuery($sql, array $params = null, array $types = null)
     {
         $this->start = microtime(true);
 
-        $this->sql = preg_replace_callback("/\?/", function ($matches) use (&$params, &$types) {
-            $param = array_shift($params);
-            if (null === $param) {
-                return "NULL";
-            } else {
-                return "'" . $param . "'";
+        if ($params) {
+            list($sql, $params, $types) = SQLParserUtils::expandListParameters($sql, $params, $types);
+
+            $quotedParams = array();
+            foreach ($params as $typeIndex => $value) {
+                $quotedParams[] = $this->connection->quote(
+                    $value,
+                    isset($types[$typeIndex]) ? $types[$typeIndex] : \PDO::PARAM_STR
+                );
             }
-        }, $sql);
+
+            $this->sql = vsprintf(str_replace("?", "%s", $sql), $quotedParams);
+        } else {
+            $this->sql = $sql;
+        }
     }
 
     public function stopQuery()
